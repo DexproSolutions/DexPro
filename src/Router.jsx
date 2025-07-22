a4c468df4e154a5f160840004a4c5b5a745ec84c
DexproSolutions <dexprosolutions@gmail.com>
2025-07-22 23:59:32 +0530

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import ProjectRequirement from "./pages/ProjectRequirement";
 import Home from "./pages/Home";
-
+import BlogContent from "./pages/BlogContent";
 const Router = createBrowserRouter([
     {
         path:'/',
@@ -12,5 +12,9 @@ const Router = createBrowserRouter([
         path:'/project',
         element: <ProjectRequirement/>
     },
+    {
+        path:'/blog/:blogId',
+        element: <BlogContent/>
+    }
 ])
 export default Router;
